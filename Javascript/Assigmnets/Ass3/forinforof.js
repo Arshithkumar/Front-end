979f8a1c4a7dd74fb36001a8531331aa6fb1da07
Arshithkumar <arrarama2k@gmail.com>
2023-02-14 22:23:57 +0530

    first commit

---
+++ Javascript/Assigmnets/Ass3/forinforof.js
@@ -0,0 +1,28 @@
+// Task 1
+var diary=['cheese','sour cream','milk','yogurt','ice cream','milkshake']
+function logDairy(){
+    for (var prot of diary) {
+        console.log(prot);
+    }
+}
+logDairy();
+// Task 2
+const animal = {
+    canJump: true
+};
+const bird = Object.create(animal);
+bird.canFly = true;
+bird.hasFeathers = true;
+function birdCan() {
+    for (key of Object.keys(bird)) {
+        console.log(key, ": ", bird[key]);
+    }
+}
+birdCan();
+// Task 3
+function animalCan() {
+    for (prop in bird) {
+        console.log(prop + ": " + bird[prop]);
+    }
+}
+animalCan();
---
+++ Javascript/functional.js
@@ -0,0 +1,59 @@
+// perform calculater on currency
+
+var currencyone= 100;
+var currencytwo=0;
+var exchangerate=1.2;
+
+function convertCurrency(amount, rate)
+{
+    return amount * rate;
+}
+currencytwo= convertCurrency(currencyone,exchangerate);
+
+console.log(currencytwo);
+
+//1
+function getDistance(mph, h) {
+    return mph * h
+}
+var mph = 60;
+var h = 2;
+var distance = getDistance(mph, h);
+
+console.log(distance); // <====== THIS HERE!
+
+//2
+//creating an object
+var virtualPet = {
+    sleepy: true,
+    nap: function() {
+        this.sleepy = false
+    }
+}
+console.log(virtualPet.sleepy) // true
+virtualPet.nap()
+console.log(virtualPet.sleepy) // false
+
+//3
+function addTwoNums(a, b) {
+    console.log(a + b)
+}
+
+function randomNum() {
+    return Math.floor((Math.random() * 10) + 1);
+}
+function specificNum() { return 42 };
+
+var useRandom = true;
+
+var getNumber;
+
+if(useRandom) {
+    getNumber = randomNum
+} else {
+    getNumber = specificNum
+}
+
+addTwoNums(getNumber(), getNumber())
+
+
